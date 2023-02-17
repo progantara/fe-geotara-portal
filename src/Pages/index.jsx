c3f9662790ea6a63b8449fd5493158634c6eb9d5
Kazuno0101 <nunoalwi@gmail.com>
2023-02-17 18:50:04 +0700

    hero index

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import PortalTemplate from '../Component/Layout';
 
+import { IoArrowForwardSharp } from 'react-icons/io5';
+import { BsBookmark } from 'react-icons/bs';
+import BgHero from '../Assets/img/hero.jpg';
+
+const Hero = () => {
+	return (
+		<section className="flex justify-between h-screen bg-cover bg-hero-pattern py-[7.5rem] px-20">
+			<div className="flex flex-col w-3/5 pr-[17rem] text-primary">
+				<h1 className="text-6xl font-bold leading-[4.125rem] ">Lupakan kerja, Mulailah Liburan</h1>
+				<p className="my-10 text-2xl">Isilah hari-hari libur kamu dengan sesuatu yang membuat kamu tersenyum, tertawa, dan bersyukur.</p>
+				<div>
+					<button className="flex items-center px-8 py-4 text-white rounded-md bg-primary">
+						Coba 360
+						<IoArrowForwardSharp color="white" />
+					</button>
+				</div>
+				<div className="grid grid-cols-3 mt-20">
+					<div className="flex flex-col space-y-4">
+						<BsBookmark size={50} />
+						<p>
+							<b>1000+</b> Wisata
+						</p>
+					</div>
+					<div className="flex flex-col space-y-4">
+						<BsBookmark size={50} />
+						<p>
+							<b>5000+</b> Spot Foto
+						</p>
+					</div>
+					<div className="flex flex-col space-y-4">
+						<BsBookmark size={50} />
+						<p>
+							<b>2500+</b> Pengunjung Baru
+						</p>
+					</div>
+				</div>
+			</div>
+			<img src={BgHero} alt="background hero" className="w-2/5 rounded-tl-[15rem] rounded-3xl" />
+		</section>
+	);
+};
+
 const Home = () => {
 	return (
-		<>
-			<PortalTemplate>
-				<h1 className="font-DMSans">Ini dm sans</h1>
-				<h1>Ini bukan dm sans</h1>
-			</PortalTemplate>
-		</>
+		<PortalTemplate>
+			<Hero />
+		</PortalTemplate>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+	mode: 'jit',
+	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			primary: '#047857',
+		},
 		extend: {
 			fontFamily: {
 				DMSans: ['"DMSans"'],
+			},
+			backgroundImage: {
+				'hero-pattern': "url('./Assets/img/pattern/travel-pattern-2.png')",
 			},
 		},
 	},
