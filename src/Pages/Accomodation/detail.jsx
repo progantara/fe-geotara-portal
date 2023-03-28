2af93bdfa566a83f87964c64aec73e721bd40546
Faizal <faizal.abdul.tif420@polban.ac.id>
2023-03-28 20:22:03 +0700

    Update detail accomodation

--- src/Pages/Accomodation/detail.jsx
+++ src/Pages/Accomodation/detail.jsx
@@ -18,7 +18,7 @@ const AccomodationDetail = () => {
 
 	return (
 		<PortalTemplate items={items}>
-			<div className="flex flex-col px-6 py-10 bg-green-100">
+			<div className="flex flex-col px-6 py-10 bg-green-100 lg:px-32">
 				<section>
 
 					{/* Header */}
@@ -41,18 +41,18 @@ const AccomodationDetail = () => {
 					</div>
 
 					{/* Gambar */}
-					<div className='flex flex-col gap-2'>
-						<div className='w-full h-52 bg-white rounded-xl overflow-hidden relative'>
+					<div className='flex flex-col gap-2 lg:flex-row'>
+						<div className='w-full h-52 bg-white rounded-xl overflow-hidden relative lg:h-full lg:w-[70%]'>
 							<img src={IMAGES.hotel3} alt="hotel" className='w-full h-full object-cover'/>
 						</div>
-						<div className='flex flex-row justify-between'>
-							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative'>
+						<div className='flex flex-row justify-between lg:flex-col lg:w-[30%] lg:gap-2'>
+							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative lg:w-full '>
 								<img src={IMAGES.hoteldetail1} alt="hotel" className='w-full h-full object-cover'/>
 							</div>
-							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative'>
+							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative lg:w-full'>
 								<img src={IMAGES.hoteldetail2} alt="hotel" className='w-full h-full object-cover'/>
 							</div>
-							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative'>
+							<div className='w-[30%] h-32 rounded-xl overflow-hidden relative lg:w-full'>
 								<img src={IMAGES.hoteldetail3} alt="hotel" className='w-full h-full object-cover'/>
 							</div>
 						</div>
@@ -67,20 +67,20 @@ const AccomodationDetail = () => {
 							<div className='flex justify-between gap-3'>
 								
 								<div className='flex flex-col items-center'>
-									<IoFastFoodOutline className='w-5 h-5 text-yellow-300'></IoFastFoodOutline>
-									<p className='text-white text-[10px]'>Sarapan</p>
+									<IoFastFoodOutline className='w-5 h-5 lg:w-12 lg:h-12 text-yellow-300'></IoFastFoodOutline>
+									<p className='text-white text-[10px] lg:text-lg'>Sarapan</p>
 								</div>
 								<div className='flex flex-col items-center'>
-									<MdOutlineGarage className='w-5 h-5 text-yellow-300'></MdOutlineGarage>
-									<p className='text-white text-[10px]'>Garasi</p>
+									<MdOutlineGarage className='w-5 h-5 lg:w-12 lg:h-12 text-yellow-300'></MdOutlineGarage>
+									<p className='text-white text-[10px] lg:text-lg'>Garasi</p>
 								</div>
 								<div className='flex flex-col items-center'>
-									<MdOutlineShower className='w-5 h-5 text-yellow-300'></MdOutlineShower>
-									<p className='text-white text-[10px]'>Kolam Renang</p>
+									<MdOutlineShower className='w-5 h-5 lg:w-12 lg:h-12 text-yellow-300'></MdOutlineShower>
+									<p className='text-white text-[10px] lg:text-lg'>Kolam Renang</p>
 								</div>
 								<div className='flex flex-col items-center'>
-									<BiWifi className='w-5 h-5 text-yellow-300'></BiWifi>
-									<p className='text-white text-[10px]'>Wifi</p>
+									<BiWifi className='w-5 h-5 lg:w-12 lg:h-12 text-yellow-300'></BiWifi>
+									<p className='text-white text-[10px] lg:text-lg'>Wifi</p>
 								</div>
 
 							</div>
@@ -100,96 +100,98 @@ const AccomodationDetail = () => {
 							</div>
 						</div>
 
-						{/* Map */}
-						<div className='w-full h-36 bg-white rounded-lg overflow-hidden'>
-							<img src={IMAGES.map1} alt="map" className='w-full h-full object-cover' />
-						</div>
+						<div className='lg:flex lg:flex-row lg:justify-between'>
+							{/* Map */}
+							<div className='w-full h-36 bg-white rounded-lg overflow-hidden lg:h-96 lg:w-[65%]'>
+								<img src={IMAGES.map1} alt="map" className='w-full h-full object-cover' />
+							</div>
 
-						{/* Destinasi Terdekat */}
-						<div className='flex flex-col gap-2'>
-							<h3 className='font-bold'>Destinasi Terdekat</h3>
-							<div className='flex flex-col gap-2'>
-								
-								{/* Destinasi */}
-								<div className='w-full flex justify-between items-center gap-2'>
+							{/* Destinasi Terdekat */}
+							<div className='flex flex-col gap-2 lg:w-[33%]'>
+								<h3 className='font-bold'>Destinasi Terdekat</h3>
+								<div className='flex flex-col gap-2'>
+									
+									{/* Destinasi */}
+									<div className='w-full flex justify-between items-center gap-2'>
 
-									<div className='flex items-center gap-2'>
-										<div className='w-10 h-10 overflow-hidden'>
-											<img src={IMAGES.hoteldetail4} alt="4" className='w-full h-full object-cover'/>
+										<div className='flex items-center gap-2'>
+											<div className='w-10 h-10 lg:w-20 lg:h-20 overflow-hidden'>
+												<img src={IMAGES.hoteldetail4} alt="4" className='w-full h-full object-cover'/>
+											</div>
+											<div className='flex flex-col'>
+												<h4 className='font-bold text-sm lg:text-lg'>Curug Cimarinjung</h4>
+												<p className='text-[10px] lg:text-base'>Wisata</p>
+											</div>
 										</div>
-										<div className='flex flex-col'>
-											<h4 className='font-bold text-sm'>Curug Cimarinjung</h4>
-											<p className='text-[10px]'>Wisata</p>
+
+										<div className='flex flex-col items-center lg:items-end'>
+											<h4 className='text-[10px] lg:text-base'>962 meters</h4>
+											<p className='text-[10px]'>(10 Minute)</p>
 										</div>
+
 									</div>
+									
+									{/* Destinasi */}
+									<div className='w-full flex justify-between items-center gap-2'>
 
-									<div className='flex flex-col items-center'>
-										<h4 className='text-[10px]'>962 meters</h4>
-										<p className='text-[10px]'>(10 Minute)</p>
+										<div className='flex items-center gap-2'>
+											<div className='w-10 h-10 lg:w-20 lg:h-20 overflow-hidden'>
+												<img src={IMAGES.hoteldetail5} alt="4" className='w-full h-full object-cover'/>
+											</div>
+											<div className='flex flex-col'>
+												<h4 className='font-bold text-sm lg:text-lg'>Curug Sodong</h4>
+												<p className='text-[10px] lg:text-base'>Wisata</p>
+											</div>
+										</div>
+
+										<div className='flex flex-col items-center lg:items-end'>
+											<h4 className='text-[10px] lg:text-base'>1200 meters</h4>
+											<p className='text-[10px]'>(12 Minute)</p>
+										</div>
+
+									</div>
+									
+									{/* Destinasi */}
+									<div className='w-full flex justify-between items-center gap-2'>
+
+										<div className='flex items-center gap-2'>
+											<div className='w-10 h-10 lg:w-20 lg:h-20 overflow-hidden'>
+												<img src={IMAGES.hoteldetail6} alt="4" className='w-full h-full object-cover'/>
+											</div>
+											<div className='flex flex-col'>
+												<h4 className='font-bold text-sm lg:text-lg'>Batu Batik</h4>
+												<p className='text-[10px] lg:text-base'>Wisata</p>
+											</div>
+										</div>
+
+										<div className='flex flex-col items-center lg:items-end'>
+											<h4 className='text-[10px] lg:text-base'>873 meters</h4>
+											<p className='text-[10px]'>(8 Minute)</p>
+										</div>
+
+									</div>
+									
+									{/* Destinasi */}
+									<div className='w-full flex justify-between items-center gap-2'>
+
+										<div className='flex items-center gap-2'>
+											<div className='w-10 h-10 lg:w-20 lg:h-20 overflow-hidden'>
+												<img src={IMAGES.hoteldetail7} alt="4" className='w-full h-full object-cover'/>
+											</div>
+											<div className='flex flex-col'>
+												<h4 className='font-bold text-sm lg:text-lg'>Pantai Pasir Putih</h4>
+												<p className='text-[10px] lg:text-base'>Wisata</p>
+											</div>
+										</div>
+
+										<div className='flex flex-col items-center lg:items-end'>
+											<h4 className='text-[10px] lg:text-base'>640 meters</h4>
+											<p className='text-[10px]'>(5 Minute)</p>
+										</div>
+
 									</div>
 
 								</div>
-								
-								{/* Destinasi */}
-								<div className='w-full flex justify-between items-center gap-2'>
-
-									<div className='flex items-center gap-2'>
-										<div className='w-10 h-10 overflow-hidden'>
-											<img src={IMAGES.hoteldetail5} alt="4" className='w-full h-full object-cover'/>
-										</div>
-										<div className='flex flex-col'>
-											<h4 className='font-bold text-sm'>Curug Sodong</h4>
-											<p className='text-[10px]'>Wisata</p>
-										</div>
-									</div>
-
-									<div className='flex flex-col items-center'>
-										<h4 className='text-[10px]'>1200 meters</h4>
-										<p className='text-[10px]'>(12 Minute)</p>
-									</div>
-
-								</div>
-								
-								{/* Destinasi */}
-								<div className='w-full flex justify-between items-center gap-2'>
-
-									<div className='flex items-center gap-2'>
-										<div className='w-10 h-10 overflow-hidden'>
-											<img src={IMAGES.hoteldetail6} alt="4" className='w-full h-full object-cover'/>
-										</div>
-										<div className='flex flex-col'>
-											<h4 className='font-bold text-sm'>Batu Batik</h4>
-											<p className='text-[10px]'>Wisata</p>
-										</div>
-									</div>
-
-									<div className='flex flex-col items-center'>
-										<h4 className='text-[10px]'>873 meters</h4>
-										<p className='text-[10px]'>(8 Minute)</p>
-									</div>
-
-								</div>
-								
-								{/* Destinasi */}
-								<div className='w-full flex justify-between items-center gap-2'>
-
-									<div className='flex items-center gap-2'>
-										<div className='w-10 h-10 overflow-hidden'>
-											<img src={IMAGES.hoteldetail7} alt="4" className='w-full h-full object-cover'/>
-										</div>
-										<div className='flex flex-col'>
-											<h4 className='font-bold text-sm'>Pantai Pasir Putih</h4>
-											<p className='text-[10px]'>Wisata</p>
-										</div>
-									</div>
-
-									<div className='flex flex-col items-center'>
-										<h4 className='text-[10px]'>640 meters</h4>
-										<p className='text-[10px]'>(5 Minute)</p>
-									</div>
-
-								</div>
-
 							</div>
 						</div>
 
@@ -203,24 +205,24 @@ const AccomodationDetail = () => {
 						<div>
 							<div className='flex justify-between items-center'>
 								{/* Gambar */}
-								<div className='flex flex-col gap-2 w-[48%] h-full'>
-									<div className='w-full h-32 bg-white rounded-xl overflow-hidden relative'>
+								<div className='flex flex-col gap-2 w-[48%] h-full lg:w-[40%]'>
+									<div className='w-full h-32 lg:h-52 bg-white rounded-xl overflow-hidden relative'>
 										<img src={IMAGES.hotel3} alt="hotel" className='w-full h-full object-cover'/>
 									</div>
 									<div className='flex flex-row justify-between'>
-										<div className='w-[30%] h-16 rounded-xl overflow-hidden relative'>
+										<div className='w-[30%] h-16 lg:h-32 rounded-xl overflow-hidden relative'>
 											<img src={IMAGES.hoteldetail1} alt="hotel" className='w-full h-full object-cover'/>
 										</div>
-										<div className='w-[30%] h-16 rounded-xl overflow-hidden relative'>
+										<div className='w-[30%] h-16 lg:h-32 rounded-xl overflow-hidden relative'>
 											<img src={IMAGES.hoteldetail2} alt="hotel" className='w-full h-full object-cover'/>
 										</div>
-										<div className='w-[30%] h-16 rounded-xl overflow-hidden relative'>
+										<div className='w-[30%] h-16 lg:h-32 rounded-xl overflow-hidden relative'>
 											<img src={IMAGES.hoteldetail3} alt="hotel" className='w-full h-full object-cover'/>
 										</div>
 									</div>
 								</div>
 
-								<div className='w-[48%] h-full bg-white p-3'>
+								<div className='w-[48%] h-full bg-white p-3 lg:w-[55%]'>
 									<h4 className='text-base font-bold mb-2'>Superior Double Room, 1 Double Bed</h4>
 									<div className='flex items-center gap-2'>
 										<BiBed></BiBed>
@@ -238,7 +240,7 @@ const AccomodationDetail = () => {
 										<BiTv></BiTv>
 										<p>TV</p>
 									</div>
-									<div className='flex w-full text-sm items-center'>
+									<div className='flex w-full text-sm items-center lg:justify-between'>
 										<span className='font-bold'>Rp 2.000.000 / Malam</span>
 										<button className="flex justify-center px-2 py-2 bg-yellow-200 rounded-md">
 											<span className="text-sm font-bold text-primary">Pesan Sekarang</span>
