711d4f9e9f115bda1f6fc5aa15ebfcfda714cf2b
Faizal <faizal.abdul.tif420@polban.ac.id>
2023-03-31 10:50:02 +0700

    tour

--- src/Services/TourService.js
+++ src/Services/TourService.js
@@ -1,7 +1,31 @@
 import axios from 'axios';
 
+// export const getTopTour = () => {
+// 	return axios.get(process.env.REACT_APP_API_BASE_URL +`/api/portal/topTour`, {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	});
+// };
+
+// export const getTour = () => {
+// 	return axios.get(process.env.REACT_APP_API_BASE_URL+ `/api/portal/tour`, {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	});
+// };
+
+// export const getOneTour = (id) => {
+// 	return axios.get(process.env.REACT_APP_API_BASE_URL+ `/api/portal/tour/` + id, {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	});
+// };
+
 export const getTopTour = () => {
-	return axios.get(process.env.REACT_APP_API_BASE_URL +`/api/portal/topTour`, {
+	return axios.get(`http://127.0.0.1:8000/api/portal/topTour`, {
 		headers: {
 			Accept: 'application/json',
 		},
@@ -9,7 +33,7 @@ export const getTopTour = () => {
 };
 
 export const getTour = () => {
-	return axios.get(process.env.REACT_APP_API_BASE_URL+ `/api/portal/tour`, {
+	return axios.get(`http://127.0.0.1:8000/api/portal/tour`, {
 		headers: {
 			Accept: 'application/json',
 		},
@@ -17,7 +41,7 @@ export const getTour = () => {
 };
 
 export const getOneTour = (id) => {
-	return axios.get(process.env.REACT_APP_API_BASE_URL+ `/api/portal/tour/` + id, {
+	return axios.get(`http://127.0.0.1:8000/api/portal/tour/` + id, {
 		headers: {
 			Accept: 'application/json',
 		},
